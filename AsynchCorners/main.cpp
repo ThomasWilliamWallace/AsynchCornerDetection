7b16d502936e4586cebe51fb874cebad93340488
Thomas Wallace <trihardar@gmail.com>
2019-04-19 22:10:41 +0100

    Display first camera image

--- AsynchCorners/main.cpp
+++ AsynchCorners/main.cpp
@@ -2,10 +2,24 @@
 #include <sstream>
 #include <string>
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 int main(int argc, char** argv)
 {
 	std::ifstream infile("cam_data/shapes_translation/events.txt");  //takes form of 'timestamp x y polarity'
+
+	cv::Mat image;
+	image = cv::imread("cam_data/shapes_translation/images/frame_00000000.png", cv::IMREAD_COLOR); // Read image
+	if (image.empty()) // Check for invalid input
+	{
+		std::cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+	cv::namedWindow("Image", cv::WINDOW_AUTOSIZE); // Create a window for display.
+	imshow("Image", image); // Show our image inside it.
+	cv::waitKey(1); //allow pause to display the image
 
 	std::cout << "BEGIN\n";
 	if (infile.is_open()) {
@@ -26,7 +40,7 @@ int main(int argc, char** argv)
 		}
 		infile.close();
 		std::cout << "END\n";
-		system("pause");
+		cv::waitKey(0); // Wait for a keystroke in the window
 	}
 
 	return 0;
